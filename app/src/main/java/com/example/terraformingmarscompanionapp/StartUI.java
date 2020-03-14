19892e6dd189a2bd597ffbc0fc308cdb4f3aacaf
EddieTheCubeHead <eetu.asikainen1204@outlook.com>
2020-03-14 16:45:46 +0200

    Added player class. No functionality in class yet.

---
+++ app/src/main/java/com/example/terraformingmarscompanionapp/StartUI.java
@@ -0,0 +1,14 @@
+package com.example.terraformingmarscompanionapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class StartUI extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_start_u_i);
+    }
+}
