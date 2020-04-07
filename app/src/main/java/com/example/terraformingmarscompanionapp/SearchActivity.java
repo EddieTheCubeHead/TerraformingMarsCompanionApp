5abdc911e66a5da47513dbd110a72040e81c80b1
Alex <aleksanteri.reijo@gmail.com>
2020-04-07 14:05:08 +0300

    muutettu cardin kautta toimivaksi.

--- app/src/main/java/com/example/terraformingmarscompanionapp/SearchActivity.java
+++ app/src/main/java/com/example/terraformingmarscompanionapp/SearchActivity.java
@@ -4,29 +4,24 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-
-
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Filter;
+
 import android.widget.SearchView;
 
-import com.example.terraformingmarscompanionapp.ui.main.CardView;
-import com.example.terraformingmarscompanionapp.ui.main.RecyclerViewAdapter;
+import com.example.terraformingmarscompanionapp.ui.main.RecyclerAdapter;
 
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Map;
-import java.util.regex.Pattern;
 
-public class SearchActivity extends AppCompatActivity
+public class SearchActivity extends AppCompatActivity implements RecyclerAdapter.OnCardListener, RecyclerAdapter.OnCardLongListener
 {
     SearchView searchview;
     HashMap<String, Card> deck;
-    ArrayList<CardView> card_list;
-    private RecyclerView.Adapter adapter;
+    ArrayList<Card> card_list;
+    private RecyclerAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -34,43 +29,29 @@ public class SearchActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
 
+        //TODO tähän passataan deck intentillä.
+
         //korttilistan luominen
         card_list = new ArrayList<>();
+        /*
         for (Map.Entry<String, Card> card : deck.entrySet())
             card_list.add(new CardView((Card) card));
+            */
 
         RecyclerView recyclerview = findViewById(R.id.result_recyclerview);
-        //TODO testausta recyclerview sethasfixedsize kanssa(ilmeisesti parempi performanssi)
-        recyclerview.setHasFixedSize(true);
+        recyclerview.setHasFixedSize(true); //TODO testausta recyclerview sethasfixedsize kanssa(ilmeisesti parempi performanssi)
 
-        adapter = new RecyclerViewAdapter(card_list);
+        adapter = new RecyclerAdapter(card_list, this, this); //this viittaa implementoituun
         recyclerview.setAdapter(adapter);
 
         RecyclerView.LayoutManager layout_manager = new LinearLayoutManager(this);
         recyclerview.setLayoutManager(layout_manager);
-
-
-        //recyclerviewn täyttö
 
         searchview = (SearchView) findViewById(R.id.searchview);
 
         recyclerview.setAdapter(adapter);
 
 
-        /*
-        searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextChange(String newText) {
-                return false;
-            }
-
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                System.out.println("SEARCH TEXT SUBMIT DETECTED");
-                return false;
-            }
-        });
-         */
     }
 
     @Override
@@ -81,17 +62,16 @@ public class SearchActivity extends AppCompatActivity
         MenuItem search_item = menu.findItem(R.id.action_search);
         SearchView searchview = (SearchView) search_item.getActionView();
         searchview.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) { return false; }
-
-            @Override
-            public boolean onQueryTextChange(String search_string)
-            {
+            @Override public boolean onQueryTextSubmit(String query) { return false; }
+            @Override public boolean onQueryTextChange(String search_string) {
                 adapter.getFilter().filter(search_string);
                 return false;
             }
         });
         return true;
     }
+
+    @Override public void onCardClick(int position) { }
+    @Override public boolean onCardLongClick(int position) { return false; }
 }
 
