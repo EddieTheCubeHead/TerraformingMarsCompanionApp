5ef3930b680986af0180bda4b56a94150c8c1c75
Alex <aleksanteri.reijo@gmail.com>
2020-04-12 02:09:56 +0300

    uusi luokka

---
+++ app/src/main/java/com/example/terraformingmarscompanionapp/ui/main/ResourceDialogActivity.java
@@ -0,0 +1,23 @@
+package com.example.terraformingmarscompanionapp.ui.main;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.terraformingmarscompanionapp.R;
+
+/**
+ * Resurssienvaihtopopupin logiikka
+ */
+public class ResourceDialogActivity extends AppCompatActivity
+{
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_resource_dialog);
+
+
+
+    }
+}
