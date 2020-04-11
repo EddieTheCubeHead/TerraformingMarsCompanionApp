ea49d1dc94e5821ed43561678e5f35d1068fbf25
Alex <aleksanteri.reijo@gmail.com>
2020-04-11 18:50:39 +0300

    Hyvin WIP adapteri resurssien vaihtamiseen. en vielä edes ihan varma toteutuksesta. pääasiassa napit ja niiden muuttama resurssi häiritsee. ehkä jonkinlainen tyyppimuuttuja? vaatisi ehkä player luokan overhaulin.

---
+++ app/src/main/java/com/example/terraformingmarscompanionapp/ui/main/ResourceChangerAdapter.java
@@ -0,0 +1,67 @@
+package com.example.terraformingmarscompanionapp.ui.main;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.terraformingmarscompanionapp.CardSubclasses.Card;
+import com.example.terraformingmarscompanionapp.GameController;
+
+
+public class ResourceChangerAdapter extends RecyclerView.Adapter<ResourceChangerAdapter.ViewHolder>
+{
+    private Card card;
+    private GameController gameController;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder
+    {
+        public ViewHolder(@NonNull View item) {
+            super(item);
+            //TODO tähän kaikki findviewbyid't itemistä
+            //ideana että pitää kaiken näkyvän datan tässä.
+            //todennäkösesti resurssitägi
+        }
+    }
+
+
+
+    public ResourceChangerAdapter(Card, GameController gameController){
+        this.gameController = gameController;
+        this.card = card;
+
+    }
+
+    //implementoidut
+    @NonNull
+    @Override
+    public ResourceChangerAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View button_array = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.layout_button_array, parent, false);
+        ResourceChangerAdapter.ViewHolder button_array_holder = new ResourceChangerAdapter.ViewHolder(button_array);
+
+        //toiminnallisuuksien lisääminen
+        button_array.findViewById(R.id.button_minus5).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        return button_array_holder;
+    }
+
+
+    @Override
+    public void onBindViewHolder(@NonNull ResourceChangerAdapter.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+}
+
