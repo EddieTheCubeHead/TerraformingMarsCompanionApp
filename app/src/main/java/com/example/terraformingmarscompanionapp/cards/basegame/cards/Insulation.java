cc5d09dd3db1a796607befec745474809883ef2c
EddieTheCubeHead <eetu.asikainen1204@gmail.com>
2020-05-06 19:17:52 +0300

    Integer dialogi toimii, kunhan title on tarpeeksi lyhyt

--- app/src/main/java/com/example/terraformingmarscompanionapp/cards/basegame/cards/Insulation.java
+++ app/src/main/java/com/example/terraformingmarscompanionapp/cards/basegame/cards/Insulation.java
@@ -1,8 +1,13 @@
 package com.example.terraformingmarscompanionapp.cards.basegame.cards;
 
+import android.content.Context;
+import android.content.Intent;
+
 import com.example.terraformingmarscompanionapp.cardSubclasses.Card;
 import com.example.terraformingmarscompanionapp.game.Game;
+import com.example.terraformingmarscompanionapp.game.GameController;
 import com.example.terraformingmarscompanionapp.game.Player;
+import com.example.terraformingmarscompanionapp.ui.main.IntegerDialogActivity;
 
 public final class Insulation extends Card {
     public Insulation(Game game) {
@@ -13,7 +18,14 @@ public final class Insulation extends Card {
 
     @Override
     public void onPlay(Player player) {
-        //TODO luvun valinta UI
+        Context context = GameController.getInstance().getContext();
+        Intent intent = new Intent(context, IntegerDialogActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+        intent.putExtra(IntegerDialogActivity.CARD_NAME, this.getName());
+        intent.putExtra(IntegerDialogActivity.INTEGER_MIN, 0);
+        intent.putExtra(IntegerDialogActivity.INTEGER_MAX, player.getHeatProduction());
+        intent.putExtra(IntegerDialogActivity.TITLE, "Amount:");
+        context.startActivity(intent);
     }
 
     @Override
--- app/src/main/java/com/example/terraformingmarscompanionapp/ui/main/IntegerDialogActivity.java
+++ app/src/main/java/com/example/terraformingmarscompanionapp/ui/main/IntegerDialogActivity.java
@@ -1,6 +1,7 @@
 package com.example.terraformingmarscompanionapp.ui.main;
 
 import android.app.AlertDialog;
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -10,8 +11,8 @@ import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -26,7 +27,16 @@ public class IntegerDialogActivity extends AppCompatActivity {
 
     AlertDialog dialog;
 
-    //todo tarkat valid types
+    public static final String INTEGER_MIN = "min";
+    public static final String INTEGER_MAX = "max";
+    public static final String CARD_NAME = "card";
+    public static final String TITLE = "title";
+
+    private Integer result;
+    private Integer min;
+    private Integer max;
+    private Card card;
+    private String dialog_title;
 
     View view;
 
@@ -44,9 +54,19 @@ public class IntegerDialogActivity extends AppCompatActivity {
         LayoutInflater inflater = LayoutInflater.from(this);
         view = inflater.inflate(R.layout.dialog_integer, null);
 
+        Intent intent = getIntent();
+        min = intent.getIntExtra(INTEGER_MIN, 0);
+        max = intent.getIntExtra(INTEGER_MAX, 10);
+
+        dialog_title = intent.getStringExtra(TITLE);
+
+        card = GameController.getInstance().getGame().getAllCards().get(intent.getStringExtra(CARD_NAME));
+
         //findviewbyid't
         TextView title = view.findViewById(R.id.title);
         EditText number_field = view.findViewById(R.id.edittext_number);
+
+        title.setText(dialog_title);
 
         Button button_negative = view.findViewById(R.id.button_negative);
         Button button_positive = view.findViewById(R.id.button_positive);
@@ -72,17 +92,30 @@ public class IntegerDialogActivity extends AppCompatActivity {
 
         Window window = dialog.getWindow();
 
-        window.setLayout(  2*width / 3, WindowManager.LayoutParams.WRAP_CONTENT);
+        window.setLayout(  4*width / 5, WindowManager.LayoutParams.WRAP_CONTENT);
 
         //listenerit
         button_negative.setOnClickListener(v -> {
-            finish();
+            exit();
         });
 
         button_positive.setOnClickListener(v -> {
-            //Integer.parseInt(number_field.getText().toString().trim());
-            //todo tähän asioita
-            finish();
+            result = Integer.parseInt(number_field.getText().toString().trim());
+
+            if (result < min) {
+                Toast.makeText(getApplicationContext(), "Number too small, minimum: " + min, Toast.LENGTH_SHORT).show();
+                return;
+            } else if (result > max) {
+                Toast.makeText(getApplicationContext(), "Number too big, maximum: " + max, Toast.LENGTH_SHORT).show();
+                return;
+            }
+            card.playServerConnection(GameController.getInstance().getCurrentPlayer(), result);
+            exit();
         });
+    }
+
+    private void exit() {
+        dialog.dismiss();
+        IntegerDialogActivity.super.onBackPressed();
     }
 }
